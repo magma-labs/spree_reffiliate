c81176857c22697431a0277a28a2937896423e3c
Manuel Vidaurre <manuel.vidaurre@agiltec.com.mx>
2014-10-21 16:14:42 -0500

    Update gems for Spree 2.4.0.rc3

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -36,6 +36,22 @@ require 'spree/testing_support/url_helpers'
 # Requires factories defined in lib/spree_refiliate/factories.rb
 require 'spree_reffiliate/factories'
 
+require 'capybara/poltergeist'
+Capybara.javascript_driver = :poltergeist
+options = {
+  js_errors: false,
+  timeout: 240,
+  phantomjs_logger: StringIO.new,
+  logger: nil,
+  phantomjs_options: ['--load-images=no', '--ignore-ssl-errors=yes']
+}
+
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, options)
+end
+Capybara.default_wait_time = 10
+Capybara.default_host = 'localhost:3000'
+
 RSpec.configure do |config|
   # Customs
   config.include Rails.application.routes.url_helpers
@@ -86,6 +102,19 @@ RSpec.configure do |config|
   # After each spec clean the database.
   config.after :each do
     DatabaseCleaner.clean
+  end
+
+  config.after(:each, type: :feature) do |example| 
+    missing_translations = page.body.scan(/translation missing: #{I18n.locale}\.(.*?)[\s<\"&]/)
+    if missing_translations.any?
+      #binding.pry
+      puts "Found missing translations: #{missing_translations.inspect}"
+      puts "In spec: #{example.location}"
+    end
+    if ENV['LOCAL']  && example.exception
+      page.save_screenshot("tmp/capybara/screenshots/#{example.metadata[:description]}.png", full: true)
+      save_and_open_page
+    end
   end
 
   config.fail_fast = ENV['FAIL_FAST'] || false
--- spree_reffiliate.gemspec
+++ spree_reffiliate.gemspec
@@ -2,7 +2,7 @@
 Gem::Specification.new do |s|
   s.platform    = Gem::Platform::RUBY
   s.name        = 'spree_reffiliate'
-  s.version     = '2.3.1'
+  s.version     = '2.4.0.rc3'
   s.author      = 'Alejandro AR'
   s.email       = 'abarcadabra@gmail.com'
   s.summary     = 'Spree Affiliate and Referrals extension'
@@ -16,15 +16,16 @@ Gem::Specification.new do |s|
   s.test_files   = s.files.grep(%r{^spec/})
   s.require_path = 'lib'
 
-  s.add_dependency 'spree_core', '~> 2.3.1'
+  s.add_dependency 'spree_core', '~> 2.4.0.rc3'
 
-  s.add_development_dependency 'capybara', '~> 2.1'
+  s.add_development_dependency 'capybara', '~> 2.4.4'
   s.add_development_dependency 'coffee-rails'
   s.add_development_dependency 'database_cleaner'
   s.add_development_dependency 'factory_girl', '~> 4.4'
   s.add_development_dependency 'ffaker'
-  s.add_development_dependency 'rspec-rails',  '~> 2.13'
-  s.add_development_dependency 'sass-rails', '~> 4.0.2'
+  s.add_development_dependency 'poltergeist'
+  s.add_development_dependency 'rspec-rails',  '~> 3.1'
+  s.add_development_dependency 'sass-rails', '~> 4.0.3' 
   s.add_development_dependency 'selenium-webdriver'
   s.add_development_dependency 'simplecov'
   s.add_development_dependency 'sqlite3'
